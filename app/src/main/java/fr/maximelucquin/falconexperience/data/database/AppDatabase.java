0743c8c22e2006843c000da86d0b335d8552b293
Maxime Lucquin <maxime.lucquin@gmail.com>
2019-03-09 17:59:55 +0100

    Ajout de l'import de bdd

--- app/src/main/java/fr/maximelucquin/falconexperience/data/database/AppDatabase.java
+++ app/src/main/java/fr/maximelucquin/falconexperience/data/database/AppDatabase.java
@@ -6,6 +6,12 @@ import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.TypeConverters;
 import android.content.Context;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
 import fr.maximelucquin.falconexperience.data.Actiion;
 import fr.maximelucquin.falconexperience.data.Item;
 import fr.maximelucquin.falconexperience.data.Sequence;
@@ -41,5 +47,42 @@ public abstract class AppDatabase extends RoomDatabase {
     public static void destroyInstance() {
         INSTANCE = null;
     }
+
+    /*public boolean importDatabase(String dbPath) throws IOException {
+
+        // Close the SQLiteOpenHelper so it will commit the created empty
+        // database to internal storage.
+        close();
+        File newDb = new File(dbPath);
+        File oldDb = new File(DB_FILEPATH);
+        if (newDb.exists()) {
+            AppDatabase.copyFile(new FileInputStream(newDb), new FileOutputStream(oldDb));
+            // Access the copied database so SQLiteHelper will cache it and mark
+            // it as created.
+            AppDatabase.getAppDatabase().close();
+            return true;
+        }
+        return false;
+    }
+
+    public static void copyFile(FileInputStream fromFile, FileOutputStream toFile) throws IOException {
+        FileChannel fromChannel = null;
+        FileChannel toChannel = null;
+        try {
+            fromChannel = fromFile.getChannel();
+            toChannel = toFile.getChannel();
+            fromChannel.transferTo(0, fromChannel.size(), toChannel);
+        } finally {
+            try {
+                if (fromChannel != null) {
+                    fromChannel.close();
+                }
+            } finally {
+                if (toChannel != null) {
+                    toChannel.close();
+                }
+            }
+        }
+    }*/
 
 }
--- app/src/main/java/fr/maximelucquin/falconexperience/views/DBManage/DBManager.java
+++ app/src/main/java/fr/maximelucquin/falconexperience/views/DBManage/DBManager.java
@@ -19,16 +19,24 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URI;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import fr.maximelucquin.falconexperience.R;
 import fr.maximelucquin.falconexperience.data.Item;
+import fr.maximelucquin.falconexperience.data.database.AppDatabase;
 import fr.maximelucquin.falconexperience.views.MainActivity;
 import fr.maximelucquin.falconexperience.views.SequenceList.SequenceListActivity;
 
 public class DBManager extends AppCompatActivity {
+
+    private int DB_READ_CODE = 2;
+    private int DB_SHM_READ_CODE = 3;
+    private int DB_WAL_READ_CODE = 4;
 
     private String dbName = "falcon_experience_db";
 
@@ -46,7 +54,13 @@ public class DBManager extends AppCompatActivity {
     }
 
     public void openImportDB(View view) {
-        findDBFile();
+        findDBFile(DB_READ_CODE);
+    }
+    public void openImportDBWAL(View view) {
+        findDBFile(DB_WAL_READ_CODE);
+    }
+    public void openImportDBSHM(View view) {
+        findDBFile(DB_SHM_READ_CODE);
     }
 
     public boolean isStoragePermissionGranted() {
@@ -114,11 +128,21 @@ public class DBManager extends AppCompatActivity {
     private void exportDB(String directoryName) {
         try {
             File dbFile = new File(this.getDatabasePath(dbName).getAbsolutePath());
+            File dbFileSHM = new File(this.getDatabasePath(dbName).getAbsolutePath()+"-shm");
+            File dbFileWAL = new File(this.getDatabasePath(dbName).getAbsolutePath()+"-wal");
+            //System.out.println(this.getDatabasePath(dbName).getAbsolutePath());
             FileInputStream fis = new FileInputStream(dbFile);
+            FileInputStream fisSHM = new FileInputStream(dbFileSHM);
+            FileInputStream fisWAL = new FileInputStream(dbFileWAL);
 
             String outFileName = directoryName + File.separator +
-                    dbName + ".db";
-            System.out.println(outFileName);
+                    dbName;
+            String outFileNameSHM = directoryName + File.separator +
+                    dbName+ "-shm";
+            String outFileNameWAL = directoryName + File.separator +
+                    dbName+ "-wal";
+
+            //System.out.println(outFileName);
 
             // Open the empty db as the output stream
             OutputStream output = new FileOutputStream(outFileName);
@@ -134,13 +158,39 @@ public class DBManager extends AppCompatActivity {
             output.close();
             fis.close();
 
+            OutputStream outputSHM = new FileOutputStream(outFileNameSHM);
+
+            // Transfer bytes from the inputfile to the outputfile
+            byte[] bufferSHM = new byte[1024];
+            int lengthSHM;
+            while ((lengthSHM = fisSHM.read(bufferSHM)) > 0) {
+                outputSHM.write(bufferSHM, 0, lengthSHM);
+            }
+            // Close the streams
+            outputSHM.flush();
+            outputSHM.close();
+            fisSHM.close();
+
+            OutputStream outputWAL = new FileOutputStream(outFileNameWAL);
+
+            // Transfer bytes from the inputfile to the outputfile
+            byte[] bufferWAL = new byte[1024];
+            int lengthWAL;
+            while ((lengthWAL = fisWAL.read(bufferWAL)) > 0) {
+                outputWAL.write(bufferWAL, 0, lengthWAL);
+            }
+            // Close the streams
+            outputWAL.flush();
+            outputWAL.close();
+            fisWAL.close();
+
 
         } catch (IOException e) {
             Log.e("dbBackup:", e.getMessage());
         }
     }
 
-    public void findDBFile() {
+    public void findDBFile(int code) {
 
         // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
         // browser.
@@ -149,13 +199,14 @@ public class DBManager extends AppCompatActivity {
         // Filter to only show results that can be "opened", such as a
         // file (as opposed to a list of contacts or timezones)
         intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
 
         // Filter to show only images, using the image MIME data type.
         // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
         // To search for all documents available via installed storage providers,
         // it would be "*/*".
         intent.setType("*/*");
-        startActivityForResult(intent, 2);
+        startActivityForResult(intent, code);
     }
 
     @Override
@@ -166,7 +217,7 @@ public class DBManager extends AppCompatActivity {
         // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
         // response to some other intent, and the code below shouldn't run at all.
 
-        if (requestCode == 2 && resultCode == Activity.RESULT_OK) {
+        if ((requestCode == DB_READ_CODE || requestCode == DB_SHM_READ_CODE || requestCode == DB_WAL_READ_CODE) && resultCode == Activity.RESULT_OK) {
             // The document selected by the user won't be returned in the intent.
             // Instead, a URI to that document will be contained in the return intent
             // provided to this method as a parameter.
@@ -175,10 +226,55 @@ public class DBManager extends AppCompatActivity {
             if (resultData != null) {
                 uri = resultData.getData();
                 System.out.println(uri.toString());
-                //showImage(uri);
+                copyAttachedDatabase(uri, requestCode);
+
             }
         }
     }
 
+    private void copyAttachedDatabase(Uri uri, int code) {
+        File dbPath = new File(this.getDatabasePath(dbName).getAbsolutePath());
+        if (code == DB_SHM_READ_CODE) {
+            dbPath = new File(this.getDatabasePath(dbName).getAbsolutePath()+"-shm");
+        } else if (code == DB_WAL_READ_CODE) {
+            dbPath = new File(this.getDatabasePath(dbName).getAbsolutePath()+"-wal");
+        }
+
+        System.out.println(this.getDatabasePath(dbName).getAbsolutePath());
+        // If the database already exists, return
+        /*if (dbPath.exists()) {
+            System.out.println("DB already Exist");
+            return;
+        }*/
+
+        // Make sure we have a path to the file
+        //dbPath.getParentFile().mkdirs();
+
+        // Try to copy database file
+        try {
+            final InputStream inputStream = getContentResolver().openInputStream(uri);
+            //Log.e("InputStream Size","Size " + inputStream);
+            final OutputStream output = new FileOutputStream(dbPath);
+
+            byte[] buffer = new byte[8192];
+            int length;
+
+            while ((length = inputStream.read(buffer, 0, 8192)) > 0) {
+                output.write(buffer, 0, length);
+            }
+
+            output.flush();
+            output.close();
+            inputStream.close();
+
+
+            AppDatabase.destroyInstance();
+            AppDatabase.getAppDatabase(getApplicationContext());
+        }
+        catch (IOException e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
 
 }
